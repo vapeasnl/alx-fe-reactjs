483aa2f463644ef1b071cf8b2be04ded330dab68
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2025-09-08 14:14:26 +0100

    Implement recipe store using Zustand for state management

---
+++ recipe-sharing-app/recipeStore.js
@@ -0,0 +1,29 @@
+import create from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filterRecipes: () =>
+    set((state) => ({
+      filteredRecipes: state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      ),
+    })),
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  deleteRecipe: (recipeId) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    })),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;
