5dced43237586cf75dfdbe138f8784c61b7aa663
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2025-10-06 11:19:08 +0100

    Add files via upload

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,19 @@
+// src/App.js
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import PostsComponent from "./components/PostsComponent";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
