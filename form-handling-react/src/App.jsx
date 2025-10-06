b48b8ea5dbad6162c4b7909e9112db29d0038663
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2025-10-06 11:15:29 +0100

    Add files via upload

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.js
+import React from "react";
+import FormikForm from "./components/FormikForm";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>User Registration</h1>
+      <FormikForm />
+    </div>
+  );
+};
+
+export default App;
